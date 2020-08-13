2ff8396b5aa405306f13e3ed59500f3b2600ef3d
fiberno <indepentdent.89@gmail.com>
2020-08-13 22:26:41 +0630

    local changese

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -14,6 +14,11 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email'=> $this->email,
+            'gender' => $this->gender
+        ];
     }
 }
