90e2d497ce31f268907d5030e64ae90b224f8b8d
fiberno <indepentdent.89@gmail.com>
2020-08-13 22:31:52 +0630

    test commit

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -15,7 +15,7 @@ class User extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            //'id' => $this->id,
             'name' => $this->name,
             'email'=> $this->email,
             'gender' => $this->gender
